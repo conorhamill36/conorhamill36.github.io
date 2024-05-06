e0c80e7dcc9f7593756fcedf0e289f7da6b1fe27
Conor Hamill <conorhamill36@hotmail.com>
2024-05-06 10:56:49 +0100

    Small printing change

--- scripts/generate_travel_map.py
+++ scripts/generate_travel_map.py
@@ -55,6 +55,7 @@ def main():
 
     # saving output to assets
     output_path = "assets/folium/travel_map.html"
+    print(f"Writing output map to {output_path}")
     travel_map.save(output_path)
 
 
